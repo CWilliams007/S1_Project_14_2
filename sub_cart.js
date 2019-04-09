f78c12b8cdea241590e9743147b4b1fb1f079243
Chad Williams <CWilliams007@west-mec.org>
2019-04-09 15:22:44 -0700

    Still Not Working2
    Trying to get it to recognize ID's in the array. .include() does NOT work.

--- sub_cart.js
+++ sub_cart.js
@@ -25,15 +25,70 @@
 // step 3, onload runs function
 window.onload = setupCart;
 
+// var buttons = document.querySelectorAll('.addButton');
+// console.log(buttons);
+
+// var sandwichQty = [];
+// for (var i = 0; i < buttons.length; i++) {
+//       sandwichQty.push(0);
+// }
+
+// console.log(sandwichQty);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Three Tries of garbage code
+
 function setupCart() {
       var addButtons = document.getElementsByClassName('addButton');
+
+      // create a array containing each ID, it will check if the id is equal to the event one, adds to sandwich number then
+
+      // global variable, array containing ITEMS in cart
+      var sandwichIDs = [];
       for (var i = 0; i < addButtons.length; i++) {
+            var sandwich;
             addButtons[i].addEventListener('click', function (e) {
-                  var sandwich = e.target.nextElementSibling.cloneNode(true);
-                  console.log(sandwich);
+                  // gets the ID of the ITEM
                   var sandwichID = e.target.nextElementSibling.id;
-                  console.log(sandwichID);
-                  var sandwichNumber = 1;
+                  sandwich = e.target.nextElementSibling;
+
+                  // runs through ID's array, if ID already inside adds to number and disables cloning
+                  // else it adds ID to array and clones sandwich
+                  // console.log(sandwichIDs)
+                  // for (var i = 0; i < sandwichIDs.length + 1; i++) {
+                  //       var sandwichNumber = 1;
+                  //       if (sandwichIDs[i] = sandwichID) {
+                  //             sandwichNumber++;
+                  //             sandwich = e.target.nextElementSibling.cloneNode(true);
+                  //       } else {
+                  //             sandwichIDs.push(sandwichID);
+                  //             sandwich = e.target.nextElementSibling.cloneNode(false);
+                  //       }
+                  // }
+                  var sandwichNumber = 0;
+                  var checkID = sandwichIDs.includes(sandwichID);
+                  console.log(checkID);
+                  if (checkID = false) {
+                        sandwichIDs.push(sandwichID);
+                        sandwich = e.target.nextElementSibling.cloneNode(false);
+                  } else {
+                        sandwichNumber++;
+                        sandwich = e.target.nextElementSibling.cloneNode(true);
+                        sandwichIDs.push(sandwichID)
+                  }
+                  console.log(sandwichIDs);
 
 
                   var cart = document.getElementById('cart');
@@ -42,10 +97,6 @@ function setupCart() {
 
 
 
-                  var idTest = cart.includes(sandwichID);
-                  if (idTest = true) {
-                        sandwichNumber++;
-                  }
                   var span = document.createElement('span');
                   var text = document.createTextNode(sandwichNumber)
                   span.appendChild(text)
@@ -62,48 +113,48 @@ function setupCart() {
 
 
 
-function addItem(e) {
-      var foodItem = e.target.nextElementSibling;
-      var foodID = e.target.nextElementSibling.id;
-      console.log(foodID);
-      var foodDescription = foodItem.cloneNode(true);
-      console.log(foodDescription);
-      var cartBox = document.getElementById('cart');
-      console.log(cartBox);
+// function addItem(e) {
+//       var foodItem = e.target.nextElementSibling;
+//       var foodID = e.target.nextElementSibling.id;
+//       console.log(foodID);
+//       var foodDescription = foodItem.cloneNode(true);
+//       console.log(foodDescription);
+//       var cartBox = document.getElementById('cart');
+//       console.log(cartBox);
 
 
-      var number = 0;
-      for (var i = 0; i < cartBox.length - 1; i++) {
-            var span = document.createElement('span');
-            var text = document.createTextNode(number);
-            span.appendChild(number);
-            foodDescription.insertBefore(span);
-            // insert before each child node
-            // must contain number
-            // append to cartBox
-      }
+//       var number = 0;
+//       for (var i = 0; i < cartBox.length - 1; i++) {
+//             var span = document.createElement('span');
+//             var text = document.createTextNode(number);
+//             span.appendChild(number);
+//             foodDescription.insertBefore(span);
+//             // insert before each child node
+//             // must contain number
+//             // append to cartBox
+//       }
 
-      var duplicateOrder = false;
+//       var duplicateOrder = false;
 
-      for (var i = 0; i < cartBox.childNodes; i++) {
-            if ([i].id = foodID) {
-                  number++;
-                  break;
-            }
-      }
+//       for (var i = 0; i < cartBox.childNodes; i++) {
+//             if ([i].id = foodID) {
+//                   number++;
+//                   break;
+//             }
+//       }
 
-      console.log(number)
+//       console.log(number)
 
-      if (duplicateOrder = false) {
-            var orderCount = document.createElement('span');
-            orderCount.appendChild(number);
-            foodDescription.appendChild(orderCount);
-            cartBox.appendChild(foodDescription);
-      }
+//       if (duplicateOrder = false) {
+//             var orderCount = document.createElement('span');
+//             orderCount.appendChild(number);
+//             foodDescription.appendChild(orderCount);
+//             cartBox.appendChild(foodDescription);
+//       }
 
 
 
-}
+// }
 
 
 
